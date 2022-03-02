407883addc9338c7cc3c692ff3803c7bee1b2201
Unique Maharjan <87868179+UniqueMaharjan@users.noreply.github.com>
2022-03-02 21:47:34 +0545

    Update settings.py

--- resumebuilder/settings.py
+++ resumebuilder/settings.py
@@ -131,8 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test.reply77@gmail.com'
-EMAIL_HOST_PASSWORD = 'HiTleX@77'
+EMAIL_HOST_USER = 'Your email here'
+EMAIL_HOST_PASSWORD = 'Your password here'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL =False
